854b1f0a919867a93e6ba55d7c67631b8e811115
Hassan <7soon721@gmail.com>
2026-07-07 14:09:28 +0300

    Fix typo in zero output string

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
@@ -16,17 +16,19 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	if (n < 0)
-    {
-        printf("%d is negative\n", n);
-    }
-    else if (n == 0)
-    {
-        printf("%d is nero\n", n);
-    }
-    else
-    {
-        printf("%d is positive\n", n);
-    }
+	{
+		printf("%d is negative\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is positive\n", n);
+	}
+
 	return (0);
 }
