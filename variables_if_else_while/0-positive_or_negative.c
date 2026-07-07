0c3781573ae671d1cfcef0fa05e6adbcd71a0c7d
Hassan <7soon721@gmail.com>
2026-07-07 14:00:52 +0300

    Add variables_if_else_while directory with its task file and README file

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Determines if a randomly generated number
+ * is positive, negative, or zero.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n < 0)
+    {
+        printf("%d is Negative\n", n);
+    }
+    else if (n == 0)
+    {
+        printf("%d is Zero\n", n);
+    }
+    else
+    {
+        printf("%d is Positive\n", n);
+    }
+	return (0);
+}
