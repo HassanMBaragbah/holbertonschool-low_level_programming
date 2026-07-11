c53148651c77bbee095bb24d8a23e95fdfd3702f
Hassan <7soon721@gmail.com>
2026-07-11 23:39:09 +0300

    Add task 102 file, update README file

---
+++ functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the first 50 Fibonacci numbers, starting with 1 and 2,
+ * followed by a new line. Numbers are separated by a comma and a space.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int count;
+	unsigned long fib1 = 1, fib2 = 2, next;
+
+	printf("%lu, %lu", fib1, fib2);
+
+	for (count = 3; count <= 50; count++)
+	{
+		next = fib1 + fib2;
+		printf(", %lu", next);
+
+		fib1 = fib2;
+		fib2 = next;
+	}
+
+	printf("\n");
+
+	return (0);
+}
