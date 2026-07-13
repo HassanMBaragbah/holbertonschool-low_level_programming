ba6ac3f826181e8036ca82346f4216fc055c0a05
Hassan <7soon721@gmail.com>
2026-07-13 18:48:02 +0300

    Add task 100 file, update README and main.h file

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -4,42 +4,26 @@
  * _atoi - Converts a string to an integer.
  * @s: The string to be converted.
  *
- * Return: The integer value of the converted string,
- *		 or 0 if no numbers are found.
+ * Return: The integer value of the converted string.
  */
 int _atoi(char *s)
 {
 	int i = 0;
-	int res = 0;
 	int sign = 1;
-	int is_num = 0;
+	unsigned int res = 0;
 
-	while (s[i] != '\0')
+	while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
 	{
 		if (s[i] == '-')
-		{
 			sign *= -1;
-		}
-		else if (s[i] == '+')
-		{
-			continue;
-		}
-		else if (s[i] >= '0' && s[i] <= '9')
-		{
-			is_num = 1;
-			res = (res * 10) - (s[i] - '0');
-		}
-		else if (is_num)
-		{
-			break;
-		}
 		i++;
 	}
 
-	if (sign > 0)
+	while (s[i] != '\0' && (s[i] >= '0' && s[i] <= '9'))
 	{
-		return (-res);
+		res = (res * 10) + (s[i] - '0');
+		i++;
 	}
 
-	return (res);
+	return (res * sign);
 }
