d377445146d9c080fd1961776eb26e343e0d43c9
Hassan <7soon721@gmail.com>
2026-07-28 16:44:26 +0300

    Add task 4 file, and update README file

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * main - prints the minimum number of coins to make change
+ * main - adds positive numbers
  * @argc: number of command line arguments
  * @argv: array containing the program command line arguments
  *
@@ -10,38 +10,22 @@
  */
 int main(int argc, char *argv[])
 {
-	int cents, coins = 0;
+	int result = 0;
+	int i, j;
 
-	if (argc != 2)
+	for (i = 1; i < argc; i++)
 	{
-		printf("Error\n");
-		return (1);
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		result += atoi(argv[i]);
 	}
 
-	cents = atoi(argv[1]);
-
-	if (cents < 0)
-	{
-		printf("0\n");
-		return (0);
-	}
-
-	while (cents > 0)
-	{
-		if (cents >= 25)
-			cents -= 25;
-		else if (cents >= 10)
-			cents -= 10;
-		else if (cents >= 5)
-			cents -= 5;
-		else if (cents >= 2)
-			cents -= 2;
-		else
-			cents -= 1;
-
-		coins++;
-	}
-
-	printf("%d\n", coins);
+	printf("%d\n", result);
 	return (0);
 }
