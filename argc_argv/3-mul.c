ae5cfd49db5440df3af83ef9c6201159383f1f80
Hassan <7soon721@gmail.com>
2026-07-28 14:22:20 +0300

    Add task 3 file, and update README file

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -12,17 +12,15 @@ int main(int argc, char *argv[])
 {
 	int num1, num2, result;
 
-	if (argv[1] && argv[2])
-	{
-		num1 = atoi(argv[1]);
-		num2 = atoi(argv[2]);
-		result = num1 * num2;
-	}
-	else
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = num1 * num2;
 
 	printf("%d\n", result);
 	return (0);
