451e48fe568a04c19c4643cb45a9cdf72cb7c967
Hassan <7soon721@gmail.com>
2026-08-09 15:10:37 +0300

    Add task 6 file, update README and lists.h files

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -1,10 +1,16 @@
 #include "lists.h"
 
+/**
+ * sum_dlistint - returns the sum of all the data (n) of a dlistint_t list
+ * @head: pointer to the head of the list
+ *
+ * Return: sum of all data, or 0 if list is empty
+ */
 int sum_dlistint(dlistint_t *head)
 {
 	int sum = 0;
 
-	while (head)
+	while (head != NULL)
 	{
 		sum += head->n;
 		head = head->next;
